1d479e35b64ab670ad20f137dda652756235a118
Tirawat Owen <bamiowen@gmail.com>
2026-01-12 19:37:34 +0700

    bring back header

--- apps/public-ui2/src/routes/__root.tsx
+++ apps/public-ui2/src/routes/__root.tsx
@@ -38,7 +38,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
-        {/* <Header /> */}
+        <Header />
         {children}
         <TanStackDevtools
           config={{
