5e708e3c2929948a94d280f44a55280fbdc1e307
Atiwat Seenark <um_oom@hotmail.com>
2026-01-14 00:03:15 +0700

    update api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,8 @@
 import cors from "@elysiajs/cors"
 import openapi from "@elysiajs/openapi"
-import { Elysia } from "elysia"
+import { Agents } from "@repo/ai"
+import { Effect } from "effect"
+import { Elysia, t } from "elysia"
 
 const app = new Elysia()
   .use(cors())
@@ -9,7 +11,24 @@ const app = new Elysia()
       path: "/docs",
     }),
   )
-  .post("/new-source", () => {})
+  .post(
+    "/new-source",
+    async ({ body }) => {
+      await Effect.gen(function* () {
+        const agentSvc = yield* Agents.AgentService
+        const qaList = yield* agentSvc.extractQA(body.text).pipe(Effect.retry(agentSvc.retryN(2)))
+        // sent each qa to queue
+      }).pipe(Effect.provide(Agents.AgentService.Default), Effect.runPromise)
+    },
+    {
+      body: t.Object({
+        source: t.String(),
+        text: t.String(),
+        url: t.String(),
+        speaker: t.String(),
+      }),
+    },
+  )
   .get("/", () => "Hello Elysia")
   .listen(3001)
 
